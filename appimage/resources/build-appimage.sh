7100a9a062ba9e6bfb34b8ea306ca472538ed413
Chapman Shoop <chapman.shoop@gmail.com>
2019-02-23 18:37:47 -0800

    Fix: left out the linuxdeploy qt plugin.

--- appimage/resources/build-appimage.sh
+++ appimage/resources/build-appimage.sh
@@ -1,8 +1,10 @@
 # Build AppImage
 cd /opt/appimage/resources
 
+export QT_SELECT=qt5
 ./linuxdeploy-x86_64.AppImage --appdir AppDir \
                               -e peercoin/src/qt/peercoin-qt \
                               -d peercoin-qt.desktop \
                               -i peercoin/share/pixmaps/peercoin128.png \
+                              --plugin qt \
                               --output appimage
