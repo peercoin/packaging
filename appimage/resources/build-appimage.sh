c35e87466e48687d737c4933588eb81a1ae919d3
Chapman Shoop <chapman.shoop@gmail.com>
2019-03-02 20:52:18 -0800

    Add an appdata.xml to the AppImage

--- appimage/resources/build-appimage.sh
+++ appimage/resources/build-appimage.sh
@@ -1,6 +1,9 @@
 # Build AppImage
 cd /opt/appimage/resources
 
+mkdir -p AppDir/usr/share/metainfo
+cp peercoin-qt.appdata.xml AppDir/usr/share/metainfo/
+
 export QT_SELECT=qt5
 ./linuxdeploy-x86_64.AppImage --appdir AppDir \
                               -e peercoin/src/qt/peercoin-qt \
