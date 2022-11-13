8581496a2899e458f15be1a70ee3fb4b5e495b78
noface <noface@outlook.com>
2022-11-13 12:38:04 +0100

    docker: port build structure from docker-peercoind repo

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,26 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-if [ $(echo "$1" | cut -c1) = "-" ]; then
+if [ "$(echo "$1" | cut -c1)" = "-" ]; then
   echo "$0: assuming arguments for peercoind"
-
   set -- peercoind "$@"
 fi
 
-if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "peercoind" ]; then
-  mkdir -p "$PEERCOIN_DATA"
-  chmod 700 "$PEERCOIN_DATA"
-  chown -R peercoin "$PEERCOIN_DATA"
+if [ "$(echo "$1" | cut -c1)" = "-" ] || [ "$1" = "peercoind" ]; then
 
-  echo "$0: setting data directory to $PEERCOIN_DATA"
+  mkdir -p "$PPC_DATA"
+  chmod 700 "$PPC_DATA"
+  chown -R peercoin "$PPC_DATA"
 
-  set -- "$@" -datadir="$PEERCOIN_DATA"
+	if [[ ! -s "$PPC_DATA/peercoin.conf" ]]; then
+    cat <<-EOF > "$PPC_DATA/peercoin.conf"
+    test.rpcbind=0.0.0.0
+    main.rpcbind=0.0.0.0
+    rpcallowip=::/0
+    rpcpassword=${RPC_PASSWORD}
+    rpcuser=${RPC_USER}
+		EOF
+    chown peercoin "$PPC_DATA/peercoin.conf"
+	fi
+
+  set -- "$@" -datadir="$PPC_DATA"
 fi
 
-if [ "$1" = "peercoind" ] || [ "$1" = "peercoin-cli" ] || [ "$1" = "peercoin-tx" ] || [ "$1" = "peercoin-wallet" ]; then
+if [ "$1" = "peercoind" ] || [ "$1" = "peercoin-cli" ] || [ "$1" = "peercoin-tx" ] || [ "$1" = "peercoin-wallet" ] || [ "$1" = "peercoin-util" ]; then
   echo
   exec gosu peercoin "$@"
 fi
 
-echo
 exec "$@"
